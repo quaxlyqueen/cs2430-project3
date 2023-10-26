88d8bdd83c12cf180c0a6bb87ec10fb42c2bee29
Abbas Al-younis <abbasal-younis@Abbass-MacBook-Air.local>
2023-10-26 15:41:28 -0600

    Implemented the Experiment class with all the sorting functions also have a test client at the bottom.

--- Experiment.cs
+++ Experiment.cs
@@ -1,48 +1,64 @@
-public class Experiment
-{
-    private int id;
-    private string name;
-    private int weight;
-    private int rating;
-    private double ratio;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
 
-    public Experiment(int id, string name, int weight, int rating)
-    {
+public class Experiment {
+    private int id { get; }
+    private string experiment { get; }
+    private int weight { get; }
+    private int rating { get; }
+    private int massToRatingRatio { get; }
+
+    public Experiment(int id, string experiment, int weight, int rating) {
         this.id = id;
-        this.name = name;
+        this.experiment = experiment;
         this.weight = weight;
         this.rating = rating;
-
-        this.ratio = weight / rating;
+        massToRatingRatio = weight / rating;
     }
 
-    public int GetId()
-    {
-        return id;
+    public static List<Experiment> SortedByWeight(List<Experiment> experiments) {
+        experiments.Sort((e1, e2) => e1.weight.CompareTo(e2.weight));
+        return experiments;
     }
 
-    public string GetName()
-    {
-        return name;
+    public static List<Experiment> SortedByRating(List<Experiment> experiments) {
+        experiments.Sort((e1, e2) => e1.rating.CompareTo(e2.rating));
+        return experiments;
     }
 
-    public int GetWeight()
-    {
-        return weight;
+    public static List<Experiment> SortedByRatio(List<Experiment> experiments) {
+        experiments.Sort((e1, e2) => e1.massToRatingRatio.CompareTo(e2.massToRatingRatio));
+        return experiments;
     }
 
-    public int GetRating()
-    {
-        return rating;
+    
+    public override string ToString() {
+        return $"[{id}, {experiment}, {weight}kg, {rating}, {massToRatingRatio}]";
     }
 
-    public double GetRatio()
-    {
-        return ratio;
-    }
+    // test client
+    public static void Main(string[] args) {
+        List<Experiment> es = new List<Experiment> {
+            new Experiment(1, "Cloud Patterns", 36, 5),
+            new Experiment(2, "Solar Flares", 264, 9),
+            new Experiment(3, "Solar Power", 188, 6),
+            new Experiment(4, "Binary Stars", 203, 8),
+            new Experiment(5, "Relativity", 104, 8),
+            new Experiment(6, "Seed Viability", 7, 4),
+            new Experiment(7, "Sun Spots", 90, 2),
+            new Experiment(8, "Mice Tumors", 65, 8),
+            new Experiment(9, "Microgravity Plant Growth", 75, 5),
+            new Experiment(10, "Micrometeorites", 170, 9),
+            new Experiment(11, "Cosmic Rays", 80, 7),
+            new Experiment(12, "Yeast Fermentation", 27, 4),
+        };
 
-    public override string ToString()
-    {
-        return $"[{id}, {name}, {weight}, {rating}]";
+        Experiment.SortedByRatio(es);
+
+        foreach (Experiment ex in es) {
+            Console.WriteLine(ex);
+        }
     }
 }
